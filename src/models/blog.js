d64d24c3d9390a871fee7a8fbdcf3f55394b1f29
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-01-08 15:42:12 +0530

    create blog schema

---
+++ src/models/blog.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+    title:String,
+    description:String
+},{timestamps:true})
+
+const Blog = mongoose.models.Blogs || mongoose.model('Blogs',BlogSchema);
