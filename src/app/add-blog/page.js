c7f55e01fd9b2296d28e750319e03ad27251f63d
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-01-08 14:33:00 +0530

    add blog page main components

--- src/app/add-blog/page.js
+++ src/app/add-blog/page.js
@@ -4,6 +4,21 @@ export default function AddBlog() {
   return (
     <div>
       <h1 className='font-bold text-lg '>Add a new blog</h1>
+
+      <div className="flex flex-col gap-4">
+      <div>
+        <label>Enter blog Title</label>
+        <input name="title" placeholder="Enter blog title" />
+      </div>
+      <div>
+        <label>Enter blog description</label>
+        <textarea name="description" placeholder="Enter blog description" />
+      </div>
+
+      <div>
+        <button>Add Blog</button>
+      </div>
+      </div>
     </div>
   );
 }
